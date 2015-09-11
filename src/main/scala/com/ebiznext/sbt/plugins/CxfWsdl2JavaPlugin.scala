665663f7023aa3493922d28e67e8b7b97ccba1bb
Bert Geens <bert@lair.be>
2015-09-11 18:23:25 +0200

    convert plugin into an AutoPlugin

--- src/main/scala/com/ebiznext/sbt/plugins/CxfWsdl2JavaPlugin.scala
+++ src/main/scala/com/ebiznext/sbt/plugins/CxfWsdl2JavaPlugin.scala
@@ -1,78 +1,88 @@
 package com.ebiznext.sbt.plugins
 
-import sbt._
-import sbt.Keys._
 import java.io.File
+
+import sbt.Keys._
+import sbt._
 
 /**
  * @author stephane.manciot@ebiznext.com
  *
  */
-object CxfWsdl2JavaPlugin extends Plugin {
+object CxfWsdl2JavaPlugin extends AutoPlugin {
 
-  trait Keys {
+  override def requires = sbt.plugins.JvmPlugin
 
-    lazy val Config = config("cxf") extend(Compile) hide
+  override def trigger = allRequirements
+
+  object autoImport {
+    lazy val CxfConfig = config("cxf").hide
 
     lazy val cxfVersion = settingKey[String]("cxf version")
     lazy val wsdl2java = taskKey[Seq[File]]("Generates java files from wsdls")
-    lazy val wsdls = settingKey[Seq[cxf.Wsdl]]("wsdls to generate java files from")
-
-  }
-
-  private object CxfDefaults extends Keys {
-    val settings = Seq(
-      cxfVersion := "2.7.3",
-      libraryDependencies ++= Seq[ModuleID](
-        "org.apache.cxf" % "cxf-tools-wsdlto-core" % cxfVersion.value % Config.name,
-        "org.apache.cxf" % "cxf-tools-wsdlto-databinding-jaxb" % cxfVersion.value % Config.name,
-        "org.apache.cxf" % "cxf-tools-wsdlto-frontend-jaxws" % cxfVersion.value % Config.name
-      ),
-      wsdls := Nil
-    )
-  }
-
-  // to avoid namespace clashes, use a nested object
-  object cxf extends Keys {
+    lazy val wsdls = settingKey[Seq[Wsdl]]("wsdls to generate java files from")
 
     case class Wsdl(file: File, args: Seq[String], key: String) {
       def outputDirectory(basedir: File) = new File(basedir, key).getAbsoluteFile
     }
 
-    val settings = Seq(ivyConfigurations += Config) ++ CxfDefaults.settings ++ Seq(
-      // initialisation de la clef correspondante au répertoire source dans lequel les fichiers générés seront copiés
-      sourceManaged in Config := sourceManaged.value / "cxf",
-      // ajout de ce répertoire dans la liste des répertoires source à prendre en compte lors de la compilation
-      managedSourceDirectories in Compile += {(sourceManaged in Config).value},
-      managedClasspath in wsdl2java <<= (classpathTypes in wsdl2java, update) map { (ct, report) =>
-          Classpaths.managedJars(Config, ct, report)
-      },
-      // définition de la tâche wsdl2java
-      wsdl2java := {
-        val s: TaskStreams = streams.value
-        val classpath : String = (((managedClasspath in wsdl2java).value).files).map(_.getAbsolutePath).mkString(System.getProperty("path.separator"))
-        val basedir : File = target.value / "cxf"
-        IO.createDirectory(basedir)
-        wsdls.value.par.foreach { wsdl =>
-          val output : File = wsdl.outputDirectory(basedir)
-          if(wsdl.file.lastModified() > output.lastModified()) {
-            val id : String = wsdl.key
-            val args : Seq[String] = Seq("-d", output.getAbsolutePath, "-verbose", "-autoNameResolution", "-exsh", "true", "-fe", "jaxws21", "-client") ++ wsdl.args :+ wsdl.file.getAbsolutePath
-            s.log.debug("Removing output directory for " + id + " ...")
-            IO.delete(output)
-            s.log.info("Compiling " + id)
-            val cmd = Seq("java", "-cp", classpath, "-Dfile.encoding=UTF-8", "org.apache.cxf.tools.wsdlto.WSDLToJava") ++ args
-            s.log.debug(cmd.toString())
-            cmd ! s.log
-            s.log.info("Finished " + id)
-          }
-          else{
-            s.log.debug("Skipping " + wsdl.key)
-          }
-          IO.copyDirectory(output, (sourceManaged in Config).value, true)
-        }
-        ((sourceManaged in Config).value ** "*.java").get
-      },
-      sourceGenerators in Compile <+= wsdl2java)
   }
+
+  import autoImport._
+
+  val cxfDefaults: Seq[Def.Setting[_]] = Seq(
+    cxfVersion := "2.7.3",
+    libraryDependencies ++= Seq(
+      "org.apache.cxf" % "cxf-tools-wsdlto-core" % cxfVersion.value % CxfConfig.name,
+      "org.apache.cxf" % "cxf-tools-wsdlto-databinding-jaxb" % cxfVersion.value % CxfConfig.name,
+      "org.apache.cxf" % "cxf-tools-wsdlto-frontend-jaxws" % cxfVersion.value % CxfConfig.name
+    ),
+    wsdls := Nil
+  )
+
+  private lazy val cxfConfig = Seq(
+    // initialisation de la clef correspondante au répertoire source dans lequel les fichiers générés seront copiés
+    sourceManaged in CxfConfig := sourceManaged.value / "cxf",
+    // ajout de ce répertoire dans la liste des répertoires source à prendre en compte lors de la compilation
+    managedSourceDirectories in Compile += {
+      (sourceManaged in CxfConfig).value
+    },
+    managedClasspath in wsdl2java <<= (classpathTypes in wsdl2java, update).map { (ct, report) =>
+      Classpaths.managedJars(CxfConfig, ct, report)
+    },
+    // définition de la tâche wsdl2java
+    wsdl2java := {
+      val s: TaskStreams = streams.value
+      println(s"managedClasspath ${managedClasspath.in(wsdl2java).value}")
+      println(s"sourceManaged: ${sourceManaged.value}")
+      val classpath: String = (((managedClasspath in wsdl2java).value).files).map(_.getAbsolutePath).mkString(System.getProperty("path.separator"))
+      println(s"classpath: ${classpath}")
+      val basedir: File = target.value / "cxf"
+      println(s"basedir $basedir")
+      IO.createDirectory(basedir)
+      wsdls.value.par.foreach { wsdl =>
+        val output: File = wsdl.outputDirectory(basedir)
+        if (wsdl.file.lastModified() > output.lastModified()) {
+          val id: String = wsdl.key
+          val args: Seq[String] = Seq("-d", output.getAbsolutePath, "-verbose", "-autoNameResolution", "-exsh", "true", "-fe", "jaxws21", "-client") ++ wsdl.args :+ wsdl.file.getAbsolutePath
+          s.log.debug("Removing output directory for " + id + " ...")
+          IO.delete(output)
+          s.log.info("Compiling " + id)
+          val cmd = Seq("java", "-cp", classpath, "-Dfile.encoding=UTF-8", "org.apache.cxf.tools.wsdlto.WSDLToJava") ++ args
+          s.log.debug(cmd.toString())
+          cmd ! s.log
+          s.log.info("Finished " + id)
+        }
+        else {
+          s.log.debug("Skipping " + wsdl.key)
+        }
+        IO.copyDirectory(output, (sourceManaged in CxfConfig).value, true)
+      }
+      ((sourceManaged in CxfConfig).value ** "*.java").get
+    },
+    sourceGenerators in Compile <+= wsdl2java
+  )
+
+  override lazy val projectSettings =
+    Seq(ivyConfigurations += CxfConfig) ++ cxfDefaults ++ inConfig(Compile)(cxfConfig)
 }
